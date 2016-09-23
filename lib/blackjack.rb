d19a19b7b9d8950b6ae941fffb05fa627fa99952
Eoghan Leddy <eoghan.leddy@flatironschool.com>
2016-09-23 18:42:28 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,50 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(num)
+  puts "Your cards add up to #{num}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  input = gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(total)
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
-def initial_round
-  # code #initial_round here
+def initial_round(array = [])
+  2.times { array << deal_card }
+  card_total = 0
+  array.each { |card| card_total += card }
+  display_card_total(card_total)
+  card_total
 end
 
-def hit?
-  # code hit? here
+def hit?(int)
+  prompt_user
+  input = get_user_input
+  card_total = int
+  unless(input == "h" || input == "s")
+    invalid_command
+    prompt_user
+  end
+  input == "h" ? card_total += deal_card : nil
+  card_total
+
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command."
 end
 
 #####################################################
@@ -39,6 +52,17 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  card_arr = []
+  card_total = 0
+  welcome
+  initial_round(card_arr)
+  card_arr.each{ |x| card_total += x }
+  until(card_total > 21)
+
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+
+  end
+  end_game(card_total)
+
 end
-    
